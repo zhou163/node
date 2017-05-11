e032bc3e6bfef156f9b41ce813de8d139791b6cc
周峰 <zhoufeng@zhoufengdeMacBook-Pro.local>
2017-05-11 16:16:10 +0800

    a

--- one/config.js
+++ one/config.js
@@ -1,7 +1,7 @@
 var rc = require('rc');
 
 module.exports = rc('one', {
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 80,
   devPort: process.env.DEV_PORT || 8181,
   db: {
     uri: process.env.RELAX_MONGO_URI || 'mongodb://localhost/one'
